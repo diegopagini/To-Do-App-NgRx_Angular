8a187f4184278ee2c37738846e86263f1b130183
Diego Pagini <dq@qbkconsulting.com>
2021-01-17 19:10:02 -0300

    componentes separados y listo para instalar ngrx

--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -6,12 +6,15 @@ import { TodoItemComponent } from './todo-item/todo-item.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { TodoPageComponent } from './todo-page/todo-page.component';
 
-
-
 @NgModule({
-  declarations: [TodoAddComponent, TodoFooterComponent, TodoItemComponent, TodoListComponent, TodoPageComponent],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    TodoAddComponent,
+    TodoFooterComponent,
+    TodoItemComponent,
+    TodoListComponent,
+    TodoPageComponent,
+  ],
+  imports: [CommonModule],
+  exports: [TodoPageComponent],
 })
-export class TodosModule { }
+export class TodosModule {}
